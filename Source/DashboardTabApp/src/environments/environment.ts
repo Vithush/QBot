545785920f716e69125cf6785c350f2e6a4983f0
Vithush <vithush.suthakaran@gmail.com>
2020-09-02 20:14:39 +0100

    Update environment.ts

--- Source/DashboardTabApp/src/environments/environment.ts
+++ Source/DashboardTabApp/src/environments/environment.ts
@@ -1,9 +1,9 @@
 export const environment = {
     production: false,
-    apiBaseUrl: "https://qbotdemotesting.azurewebsites.net/api/Request/",
-
+    apiBaseUrl: "https://presalesbot.azurewebsites.net/api/Request/",
+    
     authConfig: {
-        instance: "https://presalesbot.azurewebsites.net/api/Request/",
+        instance: "https://login.microsoftonline.com/",
         tenantId: "b39138ca-3cee-4b4a-a4d6-cd83d9dd62f0",
         clientId: "22f82f6e-5e86-422d-be63-d1c09ce0a8d5",
         redirectUri: "/app-silent-end",
